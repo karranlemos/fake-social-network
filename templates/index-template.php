e8237f454457e98869de8aed97fcf20742f35fba
Karran Lemos <karranlemos.uni@gmail.com>
2020-08-16 21:29:43 -0300

    Create basic welcome page

--- templates/index-template.php
+++ templates/index-template.php
@@ -12,7 +12,16 @@ else {
       
       <div class="split-page">
         <section class="relevant">
-          
+          <div class="central-options">
+            <header>
+              <h1>Fakebook</h1>
+              <p>Welcome back!</p>
+            </header>
+            <div class="account-buttons">
+              <button type="button" class="primary">Login</button>
+              <button type="button" class="secondary">Register</button>
+            </div>
+          </div>
         </section>
         <section class="optional">
           
