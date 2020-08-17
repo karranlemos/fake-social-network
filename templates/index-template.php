7f8950a35b6f6b80cdde1780607e88b9ca324945
Karran Lemos <karranlemos.uni@gmail.com>
2020-08-16 22:30:02 -0300

    Add non-functional modal and content

--- templates/index-template.php
+++ templates/index-template.php
@@ -14,7 +14,7 @@ else {
         <section class="relevant">
           <div class="central-options">
             <header>
-              <h1>Fakebook</h1>
+              <h1 class="logo">Fakebook</h1>
               <p>Welcome back!</p>
             </header>
             <div class="account-buttons">
@@ -28,6 +28,42 @@ else {
         </section>
       </div>
 
+    </div>
+    
+    <div class="modal">
+      <div class="modal-popup">
+        <header>
+          <button class="close"></button>
+        </header>
+        <section class="modal-content-container">
+
+          <section class="" data-modal-type="login">
+            <form action="index.php" method="post" class="full-width-form">
+              <input type="text" name="username" placeholder="Username" class="input-block">
+              <input type="password" name="password" placeholder="Password" class="input-block">
+              <div class="checkbox-container">
+                <label>Remember me? <input type="checkbox" name="remember"></label>
+              </div>
+              <button type="submit" class="primary button-block">Login</button>
+              <div class="secondary-links">
+                <a href="#">Forgot Your Password?</a>
+              </div>
+            </form>
+          </section>
+
+          <section class="" data-modal-type="register">
+            <form action="index.php" method="post" class="full-width-form">
+              <input type="text" name="username" placeholder="Username" class="input-block">
+              <input type="email" name="email" placeholder="Email" class="input-block">
+              <input type="password" name="password" placeholder="Password" class="input-block">
+              <input type="password" name="password-repeat" placeholder="Repeat Password" class="input-block">
+              
+              <button type="submit" class="primary button-block">Register</button>
+            </form>
+          </section>
+
+        </section>
+      </div>
     </div>
 
   <?php });
