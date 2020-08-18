fa4f80c3691b9cd7a367245c5bb9219d28c5f170
Karran Lemos <karranlemos.uni@gmail.com>
2020-08-18 04:17:23 -0300

    Remove unused call to Posts::get_posts()

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -12,6 +12,5 @@ $template = new Template(__DIR__.'/templates/dashboard-template.php');
 $template
   ->set('title', 'Fakebook - Your fake social network')
   ->set('base', __BASE__)
-  ->set('posts', (new Posts)->get_posts(1, 10))
 ;
 echo $template->get_page();
