fdd98ef2e1a9148d23ea06e17983ce9d6082460f
Karran Lemos <karranlemos.uni@gmail.com>
2020-08-17 23:04:44 -0300

    Undo redirect to 404, loads 404 template instead; redirects "/" to "/welcome/"

--- config/router.php
+++ config/router.php
@@ -1,11 +1,14 @@
 <?php
 
 const ROUTES = [
-  '/' => 'pages/welcome.php',
   '/welcome/' => 'pages/welcome.php',
   '/dashboard/' => 'pages/dashboard.php',
   // '/profile/' => 'pages/profile.php',
   '/messages/404/' => 'pages/404.php',
+];
+
+const REDIRECTS = [
+  '/' => '/welcome/',
 ];
 
 $route = $_SERVER['REQUEST_URI'];
@@ -13,7 +16,7 @@ $uri_info = pathinfo($route);
 if ($route[strlen($route)-1] !== '/')
   $route .= '/';
 
-if (array_key_exists($route, ROUTES))
-  require_once (ROUTES[$route]);
-else
-  Helpers::redirect('/messages/404/');
+if (array_key_exists($route, REDIRECTS))
+  Helpers::redirect(REDIRECTS[$route]);
+
+require_once (ROUTES[$route] ?? ROUTES['/messages/404/']);
