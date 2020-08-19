c3d27dde73e44165df438f79f18a91e25952cbbf
Karran Lemos <karranlemos@gmail.com>
2020-08-18 22:53:17 -0300

    Fix #16 Check if user exists before login

--- pages/requests/user/login.php
+++ pages/requests/user/login.php
@@ -1,16 +1,22 @@
 <?php
 
 if (isset($_SESSION['username'])) {
-  Helpers::return_request_code(403, "403 (Forbidden): The user is already logged in.");
+  Helpers::return_request_code(403, "403 (Forbidden): the user is already logged in.");
 }
 
 if (!isset($_POST['username']) || !isset($_POST['password'])) {
   Helpers::return_request_code(422, "422 (Unprocessable Entity): 'username', 'password' missing.");
 }
 
-$correct_password = (new Users)->check_user_password($_POST['username'], $_POST['password']);
+$users = new Users;
+
+if (!$users->check_user_exists($_POST['username'])) {
+  Helpers::return_request_code(401, "401 (Unauthorized): username doesn't exist.");
+}
+
+$correct_password = $users->check_user_password($_POST['username'], $_POST['password']);
 if (!$correct_password) {
-  Helpers::return_request_code(401, "401 (Unauthorized): Wrong credentials.");
+  Helpers::return_request_code(401, "401 (Unauthorized): wrong password.");
 }
 
 if (isset($_POST['remember'])) {
