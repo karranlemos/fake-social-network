542e4a7ee48a2ec194f6eb9f13be861632526e41
Karran Lemos <karranlemos@gmail.com>
2020-08-22 05:09:38 -0300

    Reword error messages of the delete post route

--- pages/requests/posts/delete.php
+++ pages/requests/posts/delete.php
@@ -12,15 +12,15 @@ $posts = Posts::get_instance();
 $username_from_post_id = $posts->get_username_from_post($_POST['post-id']);
 
 if ($username_from_post_id === false) {
-  Helpers::return_request_json_message(400, "400 (Bad Request): post doesn't exist");
+  Helpers::return_request_json_message(400, "Post doesn't exist");
 }
 
 if ($_SESSION['username'] !== $username_from_post_id) {
-  Helpers::return_request_json_message(401, "401 (Unauthorized): cannot delete posts from other users");
+  Helpers::return_request_json_message(401, "Cannot delete posts from other users");
 }
 
 if (!$posts->delete_post($_POST['post-id'])) {
-  Helpers::return_request_json_message(500, "500 (Internal Server Error): couldn't delete post");
+  Helpers::return_request_json_message(500, "Couldn't delete post");
 }
 
 Helpers::return_request_json_message(200, "Post deleted successfully");
